75f136242ef34ce556746d91d64946f34c825858
Dhuan Oliveira <dhuanco@gmail.com>
2020-12-08 21:08:28 +0100

    Test structure improvements: reduce duplication; create abstractions

---
+++ tests/utils.tsx
@@ -0,0 +1,138 @@
+import '@testing-library/jest-dom'
+import React from "react"
+import { withForm, FormFieldType, FormProps, fieldCollection, FormField, FormValue, parseCheckboxFormValue } from "../src/yafo"
+import {render, fireEvent} from '@testing-library/react'
+
+export enum TestForm { FirstName, LastName, Country, Gender, Hobbies }
+
+const TestComponent = ({ form, callback, getForm }: { form: FormProps<TestForm>, callback: VoidFunction, getForm?: VoidFunction }) => {
+    if (getForm)
+        getForm(form)
+
+    return (
+        <div>
+            <h1>My Form</h1>
+
+            <div id="field-first-name">
+                { form.fieldComponents.get(TestForm.FirstName) }
+            </div>
+
+            <div id="field-last-name">
+                { form.fieldComponents.get(TestForm.LastName) }
+            </div>
+
+            <div id="field-country">
+                { form.fieldComponents.get(TestForm.Country) }
+            </div>
+
+            <div id="field-gender">
+                { form.fieldComponents.get(TestForm.Gender) }
+            </div>
+
+            <div id="field-hobbies">
+                { form.fieldComponents.get(TestForm.Hobbies) }
+            </div>
+
+            <input
+                data-testid="submit"
+                type="button"
+                value="Click me"
+                onClick={() => callback({
+                    firstName: form.formValue(TestForm.FirstName),
+                    lastName: form.formValue(TestForm.LastName),
+                    country: form.formValue(TestForm.Country),
+                    gender: form.formValue(TestForm.Gender),
+                    hobbies: form.formValue(TestForm.Hobbies),
+                })}
+            />
+        </div>
+    );
+}
+
+const formFields = (): FormField<TestForm>[] => [
+    {
+        id        : TestForm.FirstName,
+        label     : "First name",
+        type      : FormFieldType.TEXT,
+        valid     : (text: FormValue) => [ /[a-zA-Z{3,10}]/.test(text as string), "Wrong first name, pal!" ],
+        initial   : "",
+        disabled  : false,
+    },
+    {
+        id        : TestForm.LastName,
+        label     : "First name",
+        type      : FormFieldType.TEXT,
+        valid     : (text: FormValue) => [ /[a-zA-Z{3,10}]/.test(text as string), "Wrong first name, pal!" ],
+        initial   : "",
+        disabled  : false,
+    },
+    {
+        id        : TestForm.Country,
+        label     : "Country",
+        type      : FormFieldType.SELECT,
+        valid     : (chosenCountry: FormValue) => [ chosenCountry > 0, "Choose a country!" ],
+        initial   : 0,
+        disabled  : false,
+        options   : { selectOptions: ["Choose a country", "Brazil", "Germany"]  }
+    },
+    {
+        id        : TestForm.Gender,
+        label     : "Gender",
+        type      : FormFieldType.RADIO,
+        valid     : (chosenGender: FormValue) => [ chosenGender > -1, "Choose a gender!" ],
+        initial   : -1,
+        disabled  : false,
+        options   : { radioOptions: ["Male", "Female"]  }
+    },
+    {
+        id        : TestForm.Hobbies,
+        label     : "Hobbies",
+        type      : FormFieldType.CHECKBOX,
+        valid     : (chosenHobbies: FormValue) => [ parseCheckboxFormValue(chosenHobbies as string).length > 0, "Please choose at least one hobby." ],
+        initial   : "",
+        disabled  : false,
+        options   : { checkboxOptions: ["Soccer", "Movies", "Music", "Books"]  }
+    },
+]
+
+const change = (container: any) => (selector: string, value: string) =>
+    fireEvent.change(container.querySelector(selector), {
+        target: { value }
+    });
+
+const click = (container: any) => (selector: string) =>
+    fireEvent.click(container.querySelector(selector));
+
+const clickNth = (container: any) => (selector: string, nth: number) =>
+    fireEvent.click(container.querySelectorAll(selector)[nth]);
+
+export const getTestComponent = () =>
+    withForm(
+        "test_form",
+        fieldCollection,
+        formFields,
+        TestComponent,
+        { errorMessagesVisible: false }
+    )
+
+export const renderTestForm = () => {
+    const Component = getTestComponent()
+
+    let form = null
+
+    const getForm = (formProps) => {
+        form = formProps
+    }
+
+    const spy = jest.fn()
+
+    const { container } = render(<Component callback={spy} getForm={getForm} />)
+
+    return {
+        form: () => form,
+        change: change(container),
+        click: click(container),
+        clickNth: clickNth(container),
+        spy: () => spy,
+    }
+}
--- tests/yafo.spec.tsx
+++ tests/yafo.spec.tsx
@@ -1,148 +1,59 @@
 import '@testing-library/jest-dom'
 import React from "react"
 import renderer from 'react-test-renderer';
-import { withForm, FormFieldType, FormProps, fieldCollection, FormField, FormValue, parseCheckboxFormValue } from "../src/yafo"
-import {render, fireEvent, screen} from '@testing-library/react'
-
-enum TestForm { FirstName, LastName, Country, Gender, Hobbies }
-
-const TestComponent = ({ form, callback }: { form: FormProps<TestForm>, callback: VoidFunction }) => {
-    return (
-        <div>
-            <h1>My Form</h1>
-
-            <div id="field-first-name">
-                { form.fieldComponents.get(TestForm.FirstName) }
-            </div>
-
-            <div id="field-last-name">
-                { form.fieldComponents.get(TestForm.LastName) }
-            </div>
-
-            <div id="field-country">
-                { form.fieldComponents.get(TestForm.Country) }
-            </div>
-
-            <div id="field-gender">
-                { form.fieldComponents.get(TestForm.Gender) }
-            </div>
-
-            <div id="field-hobbies">
-                { form.fieldComponents.get(TestForm.Hobbies) }
-            </div>
-
-            <input
-                data-testid="submit"
-                type="button"
-                value="Click me"
-                onClick={() => callback({
-                    firstName: form.formValue(TestForm.FirstName),
-                    lastName: form.formValue(TestForm.LastName),
-                    country: form.formValue(TestForm.Country),
-                    gender: form.formValue(TestForm.Gender),
-                    hobbies: form.formValue(TestForm.Hobbies),
-                })}
-            />
-        </div>
-    );
-}
-
-const formFields = (): FormField<TestForm>[] => [
-    {
-        id        : TestForm.FirstName,
-        label     : "First name",
-        type      : FormFieldType.TEXT,
-        valid     : (text: FormValue) => [ /[a-zA-Z{3,10}]/.test(text as string), "Wrong first name, pal!" ],
-        initial   : "",
-        disabled  : false,
-    },
-    {
-        id        : TestForm.LastName,
-        label     : "First name",
-        type      : FormFieldType.TEXT,
-        valid     : (text: FormValue) => [ /[a-zA-Z{3,10}]/.test(text as string), "Wrong first name, pal!" ],
-        initial   : "",
-        disabled  : false,
-    },
-    {
-        id        : TestForm.Country,
-        label     : "Country",
-        type      : FormFieldType.SELECT,
-        valid     : (chosenCountry: FormValue) => [ chosenCountry > 0, "Choose a country!" ],
-        initial   : 0,
-        disabled  : false,
-        options   : { selectOptions: ["Choose a country", "Brazil", "Germany"]  }
-    },
-    {
-        id        : TestForm.Gender,
-        label     : "Gender",
-        type      : FormFieldType.RADIO,
-        valid     : (chosenGender: FormValue) => [ chosenGender > -1, "Choose a gender!" ],
-        initial   : -1,
-        disabled  : false,
-        options   : { radioOptions: ["Male", "Female"]  }
-    },
-    {
-        id        : TestForm.Hobbies,
-        label     : "Hobbies",
-        type      : FormFieldType.CHECKBOX,
-        valid     : (chosenHobbies: FormValue) => [ parseCheckboxFormValue(chosenHobbies as string).length > 0, "Please choose at least one hobby." ],
-        initial   : "",
-        disabled  : false,
-        options   : { checkboxOptions: ["Soccer", "Movies", "Music", "Books"]  }
-    },
-]
+import {fireEvent, screen} from '@testing-library/react'
+import { renderTestForm, getTestComponent, TestForm } from './utils';
 
 test("renders", () => {
-    const Component = withForm(
-        "test_form",
-        fieldCollection,
-        formFields,
-        TestComponent,
-        { errorMessagesVisible: false }
-    )
+    const Component = getTestComponent()
 
     expect(renderer.create(<Component callback={jest.fn()} />)).toMatchSnapshot();
 })
 
 test("get values", async () => {
-    const Component = withForm(
-        "test_form",
-        fieldCollection,
-        formFields,
-        TestComponent,
-        { errorMessagesVisible: false }
-    )
+    const { change, click, clickNth, spy } = renderTestForm()
 
-    const spy = jest.fn()
+    change("#field-first-name input[type=text]", "My first name");
 
-    const { container } = render(<Component callback={spy} />)
+    change("#field-last-name input[type=text]", "My last name");
 
-    fireEvent.change(container.querySelector("#field-first-name input[type=text]"), {
-        target: { value: "My first name" }
-    });
+    change("#field-country select", "2");
 
-    fireEvent.change(container.querySelector("#field-last-name input[type=text]"), {
-        target: { value: "My last name" }
-    });
+    clickNth("#field-gender input[type=radio]", 1);
 
-    fireEvent.change(container.querySelector("#field-country select"), {
-        target: { value: "2" }
-    });
+    clickNth("#field-hobbies input[type=checkbox]", 1);
 
-    fireEvent.click(container.querySelectorAll("#field-gender input[type=radio]")[1]);
+    clickNth("#field-hobbies input[type=checkbox]", 2);
 
-    fireEvent.click(container.querySelectorAll("#field-hobbies input[type=checkbox]")[1]);
+    click("[data-testid=submit]")
 
-    fireEvent.click(container.querySelectorAll("#field-hobbies input[type=checkbox]")[2]);
-
-    fireEvent.click(screen.getByTestId("submit"))
-
-    expect(spy).toHaveBeenCalledWith({
+    expect(spy()).toHaveBeenCalledWith({
         firstName: "My first name",
         lastName: "My last name",
         country: 2,
         gender: 1,
         hobbies: "1,2",
+    })
+})
+
+describe("Form Props", () => {
+    test("dirty", async () => {
+        const { form, change } = renderTestForm()
+
+        expect(form().dirty).toEqual(false);
+
+        change("#field-first-name input[type=text]", "My first name")
+
+        expect(form().dirty).toEqual(true);
+    })
+
+    test("formValue", async () => {
+        const { form, change } = renderTestForm()
+
+        expect(form().formValue(TestForm.FirstName)).toEqual("");
+
+        change("#field-first-name input[type=text]", "My first name")
+
+        expect(form().formValue(TestForm.FirstName)).toEqual("My first name");
     })
 })
